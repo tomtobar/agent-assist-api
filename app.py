f78a096874cc0e9d1cb8e0a6e2254dc4783aaff4
Tom Tobar <tomas.tobar@finalsite.com>
2025-03-20 15:34:16 -0500

    routes skeleton

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from config import app
 from models import User, ZendeskTicket, Prompt, SuggestedSolution 
 
-@app.get("/")
-def home():
-    return "Hello, Agent Assist!"
+@app.route("/login", methods=["POST"])
+def login():
+    pass
+
+@app.route("/logout", methods=["POST"])
+def logout():
+    pass
+
+@app.get("/me")
+def me():
+    pass
+
+@app.route("/query", methods=["POST"])
+def query():
+    pass
+
+@app.route("/history", methods=["POST"])
+def history():
+    pass
+
+
+    
